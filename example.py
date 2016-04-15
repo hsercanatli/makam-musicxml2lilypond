8ed75fe6a343c598cac0029acc431ccfb0365324
Sertan Senturk <contact@sertansenturk.com>
2016-04-15 06:13:35 +0200

    Delete example.py

--- example.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'hsercanatli'
-from musicxml2lilypond.musicxml2lilypond import ScoreConverter
-
-converter = ScoreConverter()
-converter.run(filename="samplescores/pesendide--sazsemaisi--aksaksemai----iii_selim.xml")
